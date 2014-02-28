ce9bc26b56aba3297a6f653309a5950ed1e950da
cerrid-dev1 <cerrid-dev1@cerrid-dev1.(none)>
2014-02-28 10:01:46 +0530

    dataset play model commit

---
+++ app/models/DataSet.java
@@ -0,0 +1,38 @@
+package models;
+
+public class DataSet {
+	
+	private  String label ;
+	private  String name ;
+	private  String type ;
+	private String filepath;
+	
+	
+	public String getFilepath() {
+		return filepath;
+	}
+	public void setFilepath(String filepath) {
+		this.filepath = filepath;
+	}
+	public String getLabel() {
+		return label;
+	}
+	public void setLabel(String label) {
+		this.label = label;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	
+
+}
